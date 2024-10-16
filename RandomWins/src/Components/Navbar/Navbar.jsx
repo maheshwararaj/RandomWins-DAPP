70e56c5ba0bfc6100d08c6e18659e76cbb7a87bc
Athil-Aj <83157921+Athil-Aj@users.noreply.github.com>
2024-10-16 15:26:17 +0530

    Added metamask wallet in navbar

--- RandomWins/src/Components/Navbar/Navbar.jsx
+++ RandomWins/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import {ethers} from 'ethers';
 import './Navbar.css'
 const Navbar = () => {
 
@@ -10,15 +9,8 @@ const Navbar = () => {
   const onConnect=async ()=>{
     if (typeof window.ethereum !== 'undefined') {
         try {
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            await window.ethereum.request({method:"eth_requestAccounts"});
-            const signer=provider.getSigner();
-            setAddress(signer.getAddress());
-            const message = "Sign this message to verify your account";
-            const signature = await signer.signMessage(message);
-            // addr.textContent= await address;
-            // let bal=await provider.getBalance(address)
-            // console.log(ethers.utils.formatEther(bal));
+            let addr= await window.ethereum.request({method:"eth_requestAccounts"});
+            setAddress(addr[0]);
         } catch (error) {
             console.error('Error connecting to MetaMask:', error);
         }
@@ -30,7 +22,7 @@ const Navbar = () => {
     <div className='navbar'>
         <h1><span>$</span>Random<span>Wins</span></h1>
         <div className="right">
-          <li><a honClick={()=>onConnect()}> {address  ? address.substring(0,10)+"..." : "Connect Wallet" }</a></li>
+          <li><a onClick={()=>onConnect()}> {address  ? address.substring(0,10)+"..." : "Connect Wallet" }</a></li>
             <li><a href="">Help</a></li>
             <li><a href="">Logout</a></li>
         </div>
