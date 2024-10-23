447ba1dfb22201bff61ae35159b007ac4fdd20e2
MAHESHWARARAJ <117579165+maheshwararaj@users.noreply.github.com>
2024-10-23 18:42:12 +0530

    Update Winner.jsx - evaluating result and the selected option

--- RandomWins/src/Components/Winner/Winner.jsx
+++ RandomWins/src/Components/Winner/Winner.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import { choices } from '../../assets/assets'
 import './Winner.css' 
 
-const Winner = ({id,setWinnerActive}) => {
-  const [winner,setWinner] = useState(choices[id])
+
+const Winner = ({result,selected,setWinnerActive}) => {
+  const [winner,setWinner] = useState(choices[result-1])
 
   return (
     <div className='winner'>
         <h1>Winner</h1>
         <img src={winner.image} alt="" />
         <h3>{winner.name}</h3>
+        <p>{selected==result ? "You WON" : "You LOSE"}</p>
         <p id='close' onClick={()=>setWinnerActive(false)}>X</p>
     </div>
   )
